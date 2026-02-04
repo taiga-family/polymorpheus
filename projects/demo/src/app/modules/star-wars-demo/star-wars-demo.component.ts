5a553e10946e8c5dadf343705962d894c6f9de84
null <taiga-family-bot@yandex.ru>
2026-02-04 08:04:45 +0000

    chore: update @taiga-ui/* deps to v0.406.0

--- projects/demo/src/app/modules/star-wars-demo/star-wars-demo.component.ts
+++ projects/demo/src/app/modules/star-wars-demo/star-wars-demo.component.ts
@@ -1,5 +1,5 @@
 import {ChangeDetectionStrategy, Component} from '@angular/core';
-import {Highlight} from 'ngx-highlightjs';
+import {HighlightAuto} from 'ngx-highlightjs';
 
 import {AbstractDemo} from '../abstract-demo';
 import * as TypeScript from '../star-wars/star-wars.component.ts?raw';
@@ -9,7 +9,7 @@ import {TabsComponent} from '../tabs/tabs.component';
 
 @Component({
     selector: 'app-star-wars-demo',
-    imports: [Highlight, TabsComponent],
+    imports: [HighlightAuto, TabsComponent],
     templateUrl: './star-wars-demo.template.html',
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
